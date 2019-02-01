0a8ec2788dc06d66ca0f10dd02b5d7130cab767a
Artyom Susalko <artem.susalko@vrpconsulting.com>
2019-02-02 00:58:41 +0300

    size window

--- main.js
+++ main.js
@@ -2,41 +2,47 @@
 
 const path = require('path')
 const { app, ipcMain } = require('electron');
+const electron = require('electron');
 
 const Window = require('./window');
 
-function main (){
-  let mainWindow = new Window({
-    file: path.join('./render', 'index.html')
-  })
-
-  let addTodoWin;
-
-// create add todo window
-ipcMain.on('add-window', () => {
-  // if addTodoWin does not already exist
-  if (!addTodoWin) {
-    // create a new add todo window
-    addTodoWin = new Window({
-      file: path.join('render/windows/save', 'window.save.html'),
-      width: 400,
-      height: 400,
-      frame: false,
-      // close with the main window
-      parent: mainWindow
+function main() {
+    console.log(electron.screen.getPrimaryDisplay());
+    let test = electron.screen.getPrimaryDisplay();
+    console.log(test.size.height);
+    let mainWindow = new Window({
+        file: path.join('./render', 'index.html'),
+        with: test.size.width / 2,
+        height: test.size.height - test.size.height * 0.5
     })
 
-    // cleanup
-    addTodoWin.on('closed', () => {
-      addTodoWin = null
+    let addTodoWin;
+
+    // create add todo window
+    ipcMain.on('add-window', () => {
+        // if addTodoWin does not already exist
+        if (!addTodoWin) {
+            // create a new add todo window
+            addTodoWin = new Window({
+                file: path.join('render/windows/save', 'window.save.html'),
+                width: 400,
+                height: 400,
+                frame: true,
+                // close with the main window
+                parent: mainWindow
+            })
+
+            // cleanup
+            addTodoWin.on('closed', () => {
+                addTodoWin = null
+            })
+        }
     })
-  }
-})
 
 }
 
 app.on('ready', main);
 
-app.on('window-all-closed', function(){
-  app.quit();
+app.on('window-all-closed', function() {
+    app.quit();
 });
--- window.js
+++ window.js
@@ -4,25 +4,25 @@ const { BrowserWindow } = require('electron')
 
 // default window settings
 const defaultProps = {
-  width: 500,
-  height: 800,
-  show: false
+    width: 500,
+    height: 600,
+    show: false
 }
 
 class Window extends BrowserWindow {
-  constructor ({ file, ...windowSettings }) {
-    // calls new BrowserWindow with these props
-    super({ ...defaultProps, ...windowSettings })
+    constructor({ file, ...windowSettings }) {
+        // calls new BrowserWindow with these props
+        super({...defaultProps, ...windowSettings })
 
-    // load the html and open devtools
-    this.loadFile(file)
-    // this.webContents.openDevTools()
+        // load the html and open devtools
+        this.loadFile(file)
+            // this.webContents.openDevTools()
 
-    // gracefully show when ready to prevent flickering
-    this.once('ready-to-show', () => {
-      this.show()
-    })
-  }
+        // gracefully show when ready to prevent flickering
+        this.once('ready-to-show', () => {
+            this.show()
+        })
+    }
 }
 
 module.exports = Window
