cd209390cc477b10c8cdbfee695002fb487a164a
Artyom Susalko <artem.susalko@vrpconsulting.com>
2019-02-04 22:29:45 +0300

    token compleate

--- classes/auth/autorization.js
+++ classes/auth/autorization.js
@@ -1,49 +1,72 @@
 'use strict'
-class Authorisation{
-    constructor(){
+class Authorisation {
+    constructor() {
         this.token = null;
-        // this.secret = 
+        this.token2 = 123;
     }
-
-    getSession(){
+    getSession() {
         var tmp;
         let xhr = new XMLHttpRequest();
-        xhr.open('POST', 'https://login.salesforce.com/services/oauth2/token');
-        xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-        
-        xhr.onreadystatechange = function() {//Call a function when the state changes.
-            if(xhr.readyState == 4 && xhr.status == 200) {
-                console.log(JSON.parse(xhr.responseText).access_token);
-                this.token = JSON.parse(xhr.responseText).access_token;
-                // console.log(tmp);
+        xhr.open('POST', 'https://login.salesforce.com/services/oauth2/token', false);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        // let param = 'grant_type=password&client_id=3MVG9d8..z.hDcPKiapQ.uS5RessdRAuilrgS0tIX3wG7SYStiS_Dasl6YeHcPhWMxSf3Fd7DrvSpEY6fTKOw&client_secret=2360969814915822973&username=volerey@vrpconsulting.com&password=S1mplepa55v2';
+        // xhr.send(param);
+        let param = 'grant_type=password&client_id=3MVG9d8..z.hDcPKiapQ.uS5RessdRAuilrgS0tIX3wG7SYStiS_Dasl6YeHcPhWMxSf3Fd7DrvSpEY6fTKOw&client_secret=2360969814915822973&username=volerey@vrpconsulting.com&password=S1mplepa55v2';
+        let test;
+        xhr.onreadystatechange = function(token) {
+            if (xhr.readyState != 4) return;
+
+            if (xhr.status != 200) {
+                // обработать ошибку
+                alert(xhr.status + ': ' + xhr.statusText);
+            } else {
+                try {
+                    test = JSON.parse(xhr.responseText);
+                    // console.log(JSON.parse(xhr.responseText));
+                } catch (e) {
+                    alert("Некорректный ответ " + e.message);
+                }
+                // setTocken(phones);
             }
         }
-
-        let param = 'grant_type=password&client_id=3MVG9d8..z.hDcPKiapQ.uS5RessdRAuilrgS0tIX3wG7SYStiS_Dasl6YeHcPhWMxSf3Fd7DrvSpEY6fTKOw&client_secret=2360969814915822973&username=volerey@vrpconsulting.com&password=S1mplepa55v2';
         xhr.send(param);
+        // let param = 'grant_type=password&client_id=3MVG9d8..z.hDcPKiapQ.uS5RessdRAuilrgS0tIX3wG7SYStiS_Dasl6YeHcPhWMxSf3Fd7DrvSpEY6fTKOw&client_secret=2360969814915822973&username=volerey@vrpconsulting.com&password=S1mplepa55v2';
+        // xhr.send(param);
         //  return tmp;
+        return test;
     }
 
-    getDialog(tocken){
+    getDialog(tocken) {
         var tmp;
-        console.log(tocken + 'dsfsdf');
+        console.log(tocken + '   ===////');
         let xhr = new XMLHttpRequest();
         xhr.open('GET', 'https://volerey-dev-ed.my.salesforce.com/services/apexrest/DialogCreator');
-        xhr.setRequestHeader('Authorization','Bearer ' + tocken);
+        xhr.setRequestHeader('Authorization', 'Bearer ' + tocken);
         // xhr.setRequestHeader("Content-type", "ap");
         // xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-        xhr.onload = function () {
+        xhr.onload = function() {
             console.log(xhr.responseText);
-          };
+        };
         xhr.send();
     }
-    getTocken(){
+
+    getTest() {
+        console.log(this.token2);
+    }
+    getTocken() {
         return this.token;
+    }
+    setTocken(tocken) {
+        console.log(tocken + '  |||||');
+        this.token = tocken;
     }
 }
 
 let test = new Authorisation();
-test.getSession();
+// test.getTest();
+console.log(test.getSession());
+test.setTocken(test.getSession().access_token);
+console.log(test.getTocken());
 test.getDialog(test.getTocken());
 // test.getTocken;
 // console.log(test.getTocken());
